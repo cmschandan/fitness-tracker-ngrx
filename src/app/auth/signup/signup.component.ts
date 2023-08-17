ec4b0256690edc18b30ec5aa632670deb8585800
cmschandan <chandan.cotocus@gmail.com>
2023-08-17 20:55:49 +0530

    adding version 1

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+}
