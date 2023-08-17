c43867068936f1c59cbe1e824c39b258cef76d66
cmschandan <chandan.cotocus@gmail.com>
2023-08-17 23:27:26 +0530

    signup page with validation added

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  constructor(){
 
+  }
+
+  ngOnInit():void {
+
+  }
+  onSubmit(form:NgForm){
+    console.log(form);
+  }
 }
